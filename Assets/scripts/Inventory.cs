f21766b4d26582b0eca640691550e95268dec068
dddushesss <37773701+dddushesss@users.noreply.github.com>
2021-04-29 19:37:32 +0300

    Task2

---
+++ Assets/scripts/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private List<IteamSO> _items;
+
+    public void PickUp(IteamSO iteamSo)
+    {
+        _items.Add(iteamSo);
+        Destroy(iteamSo.Prefab);
+    }
+
+    public void Drop(IteamSO iteamSo)
+    {
+        _items.Remove(iteamSo);
+        Instantiate(iteamSo.Prefab);
+    }
+}
---
+++ Assets/scripts/ItemController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    [SerializeField] private IteamSO Iteam;
+
+    private void Start()
+    {
+        Instantiate(Iteam.Prefab, transform);
+    }
+}
